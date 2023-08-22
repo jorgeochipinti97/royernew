5ab65617bbbb09455c4db1c2742206b4380b41a9
Jorge Ariel Ochipinti <jorge@Jorges-MacBook-Pro.local>
2023-08-22 05:10:08 -0300

    2

--- pages/cart/empty.js
+++ pages/cart/empty.js
@@ -1,37 +1,31 @@
-import NextLink from 'next/link';
+import NextLink from "next/link";
 
-import { RemoveShoppingCartOutlined } from "@mui/icons-material"
-import { Box, Link, Typography } from "@mui/material"
-import { ShopLayout } from '@/components/Layout';
-
-
-
-
+import { RemoveShoppingCartOutlined } from "@mui/icons-material";
+import { Box, Button, Link, Typography } from "@mui/material";
+import { ShopLayout } from "@/components/Layout";
 
 const EmptyPage = () => {
   return (
-    <ShopLayout title="Cart empty" pageDescription="No hay artículos en el carrito de compras">
-         <Box 
-            display='flex' 
-            justifyContent='center' 
-            alignItems='center' 
-            height='calc(100vh - 200px)'
-            sx={{ flexDirection: { xs: 'column', sm: 'row' }}}
-        >
-            <RemoveShoppingCartOutlined sx={{ fontSize: 100 }} />
-            <Box display='flex' flexDirection='column' alignItems='center'>
-
-                <NextLink href='/' passHref>
-
-                        Back
-
-                </NextLink>
-            </Box>
-
-
+    <ShopLayout
+      title="Cart empty"
+      pageDescription="No hay artículos en el carrito de compras"
+    >
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        height="calc(100vh - 200px)"
+        sx={{ flexDirection: { xs: "column", sm: "row" } }}
+      >
+        <RemoveShoppingCartOutlined sx={{ fontSize: 100 }} />
+      </Box>
+        <Box display="flex" flexDirection="column" alignItems="center">
+          <NextLink href="/" passHref>
+            <Button color="secondary">Back</Button>
+          </NextLink>
         </Box>
     </ShopLayout>
-  )
-}
+  );
+};
 
-export default EmptyPage
+export default EmptyPage;
