f38d6dda38b50c9947da0890ddf9add6069a3156
Jorge Ariel Ochipinti <jorge@Jorges-MacBook-Pro.local>
2023-09-18 04:38:47 -0300

    fixed

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import { useProduct } from "@/Hooks/UseProducts";
 // import { SectionArgentina } from "@/components/Home/UI/SectionArgentina";
 import { ShopLayout } from "@/components/Layout";
-import { gsap } from "gsap";
+import { Power1, gsap } from "gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 
 const SectionBoca = lazy(
@@ -17,9 +17,10 @@ const SectionShipping = lazy(
   async () => await import("../components/Home/UI/SectionShipping")
 );
 
-import { Box, useMediaQuery } from "@mui/material";
+import { Box, LinearProgress, useMediaQuery } from "@mui/material";
 import { Loading } from "@/components/Loading";
 import { Suspense, lazy, useEffect, useState } from "react";
+import Image from "next/image";
 
 export default function Home() {
   const isMobile = useMediaQuery("(max-width:600px)");
@@ -87,52 +88,86 @@ export default function Home() {
     ]);
   }, [riverProducts]);
 
+  useEffect(() => {
+    setTimeout(() => {
+      gsap.to(".intro", {
+        yPercent: -1000,
+        ease: Power1.easeIn,
+        duration: 1,
+      });
+    }, 1000);
+  }, []);
+
   return (
     <>
-      {productsBoca && productsBoca.length > 1 ? (
-        <>
-          <ShopLayout imageFullUrl={'/logoroyer.jpg"'} title={"Royer Store"}>
-            <Box sx={{ my: 0 }}>
-              <Suspense>
-                <SectionArgentina
-                  products={argentinaProducts.slice(0, 7)}
-                  isMobile={isMobile}
-                />
-              </Suspense>
+      <ShopLayout imageFullUrl={'/logoroyer.jpg"'} title={"Royer Store"}>
+        <Box
+          className="intro"
+          sx={{
+            height: "100vh",
+            width: "100vw",
+            position: "fixed",
+            backgroundColor: "#272843",
+            display: "flex",
+            zIndex: 100,
+            flexDirection: "column",
+            justifyContent: "start",
+            alignItems: "center",
+          }}
+        >
+          <Image
+            src="/royerpwa.png"
+            width={200}
+            height={200}
+            style={{ marginTop: 100 }}
+          />
+          <div
+            style={{
+              width: "100vw",
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            <div style={{ width: "50vw" }}>
+              <LinearProgress value={50} color="secondary" />
+            </div>
+          </div>
+        </Box>
+        <Box sx={{ my: 0 }}>
+          <Suspense>
+            <SectionArgentina
+              products={argentinaProducts.slice(0, 7)}
+              isMobile={isMobile}
+            />
+          </Suspense>
 
-              <Suspense>
-                <SectionBoca
-                  products={
-                    productsBoca[0] != undefined && productsBoca.length > 0
-                      ? productsBoca
-                      : bocaProducts
-                  }
-                  isMobile={isMobile}
-                />
-              </Suspense>
+          <Suspense>
+            <SectionBoca
+              products={
+                productsBoca[0] != undefined && productsBoca.length > 0
+                  ? productsBoca
+                  : bocaProducts
+              }
+              isMobile={isMobile}
+            />
+          </Suspense>
 
-              <Suspense>
-                <SectionRiver
-                  products={
-                    productsRiver[0] != undefined && productsRiver.length > 0
-                      ? productsRiver
-                      : bocaProducts
-                  }
-                  isMobile={isMobile}
-                />
-              </Suspense>
+          <Suspense>
+            <SectionRiver
+              products={
+                productsRiver[0] != undefined && productsRiver.length > 0
+                  ? productsRiver
+                  : bocaProducts
+              }
+              isMobile={isMobile}
+            />
+          </Suspense>
 
-              <Suspense>
-                <SectionShipping isMobile={isMobile} />
-              </Suspense>
-            </Box>
-          </ShopLayout>
-        </>
-      ) : (
-        <>
-          <Loading />
-        </>
-      )}
+          <Suspense>
+            <SectionShipping isMobile={isMobile} />
+          </Suspense>
+        </Box>
+      </ShopLayout>
     </>
   );
 }
